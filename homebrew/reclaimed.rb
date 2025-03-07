3fcd9d0f8be532eb2e0163de518a28d1af20758c
Taylor Wilsdon <tbarrettwilsdon@gmail.com>
2025-03-07 11:23:39 -0500

    Add missing typing_extensions from uv build

--- homebrew/reclaimed.rb
+++ homebrew/reclaimed.rb
@@ -3,9 +3,9 @@ class Reclaimed < Formula
 
   desc "A powerful disk usage analyzer with iCloud support"
   homepage "https://github.com/taylorwilsdon/reclaimed"
-  version "0.2.0"
+  version "0.2.1"
   url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "b1c19c57fb53c6add218efac6c00c5f74131c7dee384274f059f16d0902c07f2"
+  sha256 "2ce18e56ff5066491ab87942e6097d434d11a3c9d02648c342a78d5ad019df80"
   license "MIT"
 
   depends_on "python@3.11"
@@ -13,17 +13,17 @@ class Reclaimed < Formula
   # Runtime dependencies only - no build dependencies needed
   resource "click" do
     url "https://files.pythonhosted.org/packages/b9/2e/0090cbf739cee7d23781ad4b89a9894a41538e4fcf4c31dcdd705b78eb8b/click-8.1.8.tar.gz"
-    sha256 "b1c19c57fb53c6add218efac6c00c5f74131c7dee384274f059f16d0902c07f2"
+    sha256 "ed53c9d8990d83c2a27deae68e4ee337473f6330c040a31d4225c9574d16096a"
   end
 
   resource "rich" do
     url "https://files.pythonhosted.org/packages/ab/3a/0316b28d0761c6734d6bc14e770d85506c986c85ffb239e688eeaab2c2bc/rich-13.9.4.tar.gz"
-    sha256 "b1c19c57fb53c6add218efac6c00c5f74131c7dee384274f059f16d0902c07f2"
+    sha256 "439594978a49a09530cff7ebc4b5c7103ef57baf48d5ea3184f21d9a2befa098"
   end
 
   resource "textual" do
     url "https://files.pythonhosted.org/packages/41/62/4af4689dd971ed4fb3215467624016d53550bff1df9ca02e7625eec07f8b/textual-2.1.2.tar.gz"
-    sha256 "b1c19c57fb53c6add218efac6c00c5f74131c7dee384274f059f16d0902c07f2"
+    sha256 "aae3f9fde00c7440be00e3c3ac189e02d014f5298afdc32132f93480f9e09146"
   end
 
   def install
