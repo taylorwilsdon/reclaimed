ac4b2cfcc1f279cbb08257111ae2b29323ccebd0
Taylor Wilsdon <tbarrettwilsdon@gmail.com>
2025-07-26 13:27:06 -0400

    fix

--- homebrew/reclaimed.rb
+++ homebrew/reclaimed.rb
@@ -3,66 +3,66 @@ class Reclaimed < Formula
 
   desc "Powerful disk usage analyzer with iCloud support"
   homepage "https://github.com/taylorwilsdon/reclaimed"
-  url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v0.2.8.tar.gz"
-  sha256 "63ac4ed58630dcd78d06a2d802dbec276c4c6808c431cfdfc4f372aece2e26b3"
+  url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v0.2.9.tar.gz"
+  sha256 "0fb12a2cdfb53a9c5986a522ece9e33b3ceede49a53aa8d07b2794cfc12aa8a3"
   license "MIT"
 
   depends_on "python"
 
   # Runtime dependencies only - no build dependencies needed
   resource "click" do
-    url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v0.2.8.tar.gz"
-    sha256 "63ac4ed58630dcd78d06a2d802dbec276c4c6808c431cfdfc4f372aece2e26b3"
+    url "https://files.pythonhosted.org/packages/60/6c/8ca2efa64cf75a977a0d7fac081354553ebe483345c734fb6b6515d96bbc/click-8.2.1.tar.gz"
+    sha256 "27c491cc05d968d271d5a1db13e3b5a184636d9d930f148c50b038f0d0646202"
   end
 
   resource "rich" do
-    url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v0.2.8.tar.gz"
-    sha256 "63ac4ed58630dcd78d06a2d802dbec276c4c6808c431cfdfc4f372aece2e26b3"
+    url "https://files.pythonhosted.org/packages/fe/75/af448d8e52bf1d8fa6a9d089ca6c07ff4453d86c65c145d0a300bb073b9b/rich-14.1.0.tar.gz"
+    sha256 "e497a48b844b0320d45007cdebfeaeed8db2a4f4bcf49f15e455cfc4af11eaa8"
   end
 
   resource "textual" do
-    url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v0.2.8.tar.gz"
-    sha256 "63ac4ed58630dcd78d06a2d802dbec276c4c6808c431cfdfc4f372aece2e26b3"
+    url "https://files.pythonhosted.org/packages/ca/45/44120c661037e64b80518871a800a0bd18c13aab4b68711b774f3b9d58b1/textual-5.0.1.tar.gz"
+    sha256 "c6e20489ee585ec3fa43b011aa575f52e4fafad550e040bff9f53a464897feb6"
   end
 
   resource "typing-extensions" do
-    url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v0.2.8.tar.gz"
-    sha256 "63ac4ed58630dcd78d06a2d802dbec276c4c6808c431cfdfc4f372aece2e26b3"
+    url "https://files.pythonhosted.org/packages/98/5a/da40306b885cc8c09109dc2e1abd358d5684b1425678151cdaed4731c822/typing_extensions-4.14.1.tar.gz"
+    sha256 "38b39f4aeeab64884ce9f74c94263ef78f3c22467c8724005483154c26648d36"
   end
 
   resource "linkify-it-py" do
-    url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v0.2.8.tar.gz"
-    sha256 "63ac4ed58630dcd78d06a2d802dbec276c4c6808c431cfdfc4f372aece2e26b3"
+    url "https://files.pythonhosted.org/packages/2a/ae/bb56c6828e4797ba5a4821eec7c43b8bf40f69cda4d4f5f8c8a2810ec96a/linkify-it-py-2.0.3.tar.gz"
+    sha256 "68cda27e162e9215c17d786649d1da0021a451bdc436ef9e0fa0ba5234b9b048"
   end
 
   resource "markdown-it-py" do
-    url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v0.2.8.tar.gz"
-    sha256 "63ac4ed58630dcd78d06a2d802dbec276c4c6808c431cfdfc4f372aece2e26b3"
+    url "https://files.pythonhosted.org/packages/38/71/3b932df36c1a044d397a1f92d1cf91ee0a503d91e470cbd670aa66b07ed0/markdown-it-py-3.0.0.tar.gz"
+    sha256 "e3f60a94fa066dc52ec76661e37c851cb232d92f9886b15cb560aaada2df8feb"
   end
 
   resource "mdit-py-plugins" do
-    url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v0.2.8.tar.gz"
-    sha256 "63ac4ed58630dcd78d06a2d802dbec276c4c6808c431cfdfc4f372aece2e26b3"
+    url "https://files.pythonhosted.org/packages/19/03/a2ecab526543b152300717cf232bb4bb8605b6edb946c845016fa9c9c9fd/mdit_py_plugins-0.4.2.tar.gz"
+    sha256 "5f2cd1fdb606ddf152d37ec30e46101a60512bc0e5fa1a7002c36647b09e26b5"
   end
 
   resource "mdurl" do
-    url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v0.2.8.tar.gz"
-    sha256 "63ac4ed58630dcd78d06a2d802dbec276c4c6808c431cfdfc4f372aece2e26b3"
+    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
   end
 
   resource "platformdirs" do
-    url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v0.2.8.tar.gz"
-    sha256 "63ac4ed58630dcd78d06a2d802dbec276c4c6808c431cfdfc4f372aece2e26b3"
+    url "https://files.pythonhosted.org/packages/fe/8b/3c73abc9c759ecd3f1f7ceff6685840859e8070c4d947c93fae71f6a0bf2/platformdirs-4.3.8.tar.gz"
+    sha256 "3d512d96e16bcb959a814c9f348431070822a6496326a4be0911c40b5a74c2bc"
   end
 
   resource "pygments" do
-    url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v0.2.8.tar.gz"
-    sha256 "63ac4ed58630dcd78d06a2d802dbec276c4c6808c431cfdfc4f372aece2e26b3"
+    url "https://files.pythonhosted.org/packages/b0/77/a5b8c569bf593b0140bde72ea885a803b82086995367bf2037de0159d924/pygments-2.19.2.tar.gz"
+    sha256 "636cb2477cec7f8952536970bc533bc43743542f70392ae026374600add5b887"
   end
 
   resource "uc-micro-py" do
-    url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v0.2.8.tar.gz"
-    sha256 "63ac4ed58630dcd78d06a2d802dbec276c4c6808c431cfdfc4f372aece2e26b3"
+    url "https://files.pythonhosted.org/packages/91/7a/146a99696aee0609e3712f2b44c6274566bc368dfe8375191278045186b8/uc-micro-py-1.0.3.tar.gz"
+    sha256 "d321b92cff673ec58027c04015fcaa8bb1e005478643ff4a500882eaab88c48a"
   end
 
   def install
